a679f2f6a2d06dd085f1795bea478a2d160b1e53
iOliver678 <olivermajano01@gmail.com>
2025-09-19 11:28:17 -0700

    lab2

---
+++ week2/labs/lab1/temp.js
@@ -0,0 +1,7 @@
+try {
+  // Code that may throw an error
+  throw new Error('An error occurred!'); } 
+catch (error) {
+  // Code to handle the error
+  console.error('Caught an error:', error.message); 
+}
